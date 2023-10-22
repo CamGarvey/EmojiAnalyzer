6842eb82dfda28bdb9a03afc7e4a2c50ea8fb21d
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2023-10-22 19:37:49 +1100

    test

--- emoji_analyzer.py
+++ emoji_analyzer.py
@@ -1,21 +1,59 @@
+from typing import Union, Type
+
 from PIL import Image, ImageDraw, ImageFont
 import numpy as np
+from pilmoji import Pilmoji
+from pilmoji.source import BaseSource, AppleEmojiSource
 
 
 class EmojiAnalyzer:
 
-    def __init__(self, emoji_font_path):
-        self.emoji_font_path = emoji_font_path
+    def __init__(self, font_path, source: Union[BaseSource, Type[BaseSource]] = AppleEmojiSource):
+        self.font_path = font_path
+        self.source = source
 
-    def get_average_color(self, emoji: str, font_size=48, background: (int, int, int, int) = None):
+    def get_average_color(self, emoji: str, font_size=48, background: (int, int, int, int) = None, show=False):
+        with Image.new('RGBA', (font_size, font_size), (0, 0, 0, 255) if not background else background) as image:
+            font = ImageFont.truetype(self.font_path, font_size)
+
+            with Pilmoji(image, source=self.source) as pilmoji:
+                pilmoji.text((0, 0), emoji, (0, 0, 0), font)
+
+            # Convert the image to a NumPy array for easier manipulation
+            image_array = np.array(image)
+
+            if show:
+                image.show()
+
+            if background and background[3] != 0:
+                # Calculate the average color by taking the mean of all pixel values
+                average_color = np.mean(image_array, axis=(0, 1))
+            else:
+                # Extract non-transparent pixels
+                non_transparent_pixels = image_array[:, :, :-1][image_array[:, :, 3] > 0]
+
+                # Calculate the average color by taking the mean of non-transparent pixel values
+                if non_transparent_pixels.size > 0:
+                    average_color = np.mean(non_transparent_pixels, axis=0)
+                else:
+                    # If there are no non-transparent pixels, return transparent black
+                    average_color = (0, 0, 0)
+
+            # Convert the average color to RGB format
+            average_color = tuple(map(int, average_color))
+
+            return average_color
+
+    def get_average_color_text(self, emoji: str, padding=0, font_size=48, background: (int, int, int, int) = None, show=False):
         try:
-            container_size = font_size + 5
+            container_size = font_size + padding
             # Create a new image with a transparent background or background
-            image = Image.new("RGBA", (container_size, container_size), (0, 0, 0, 0) if not background else background)
+            image = Image.new("RGBA", (container_size, container_size),
+                              (0, 0, 0, 255) if not background else background)
             draw = ImageDraw.Draw(image)
 
             # Use a large font size to draw the emoji in the center of the image
-            font = ImageFont.truetype(self.emoji_font_path, font_size, encoding='unic')
+            font = ImageFont.truetype(self.font_path, font_size, encoding='unic')
             bbox = draw.textbbox((font_size, font_size), emoji, font=font)
             text_height = bbox[3] - bbox[1]
             text_width = bbox[2] - bbox[0]
@@ -23,9 +61,10 @@ class EmojiAnalyzer:
             y = (container_size - text_height) / 2
 
             # Draw the emoji on the image
-            draw.text((x, y + 5), emoji, embedded_color=True, font=font)
+            draw.text((x, y), emoji, embedded_color=True, font=font)
 
-            # image.show()
+            if show:
+                image.show()
             # Convert the image to a NumPy array for easier manipulation
             image_array = np.array(image)
 
--- main.py
+++ main.py
@@ -1,19 +1,60 @@
+import os.path
+from abc import abstractmethod
+from io import BytesIO
+from typing import Optional
+
 import emoji
 import json
+
+from PIL import Image
+
 from emoji_analyzer import EmojiAnalyzer
+from pilmoji.source import MicrosoftEmojiSource, BaseSource
+
+
+class FileBaseSource(BaseSource):
+
+    def get_emoji(self, emoji: str, /) -> Optional[BytesIO]:
+        """Retrieves a :class:`io.BytesIO` stream for the image of the given emoji."""
+        codes = []
+        for c in emoji:
+            code = f'{ord(c):X}'
+            if code == 'FE0F':
+                break
+            codes.append(code)
+        path = f"assets/128/"
+        prefix = "emoji_u"
+        extension = ".png"
+
+        try:
+            full = f"{path}{prefix}{'_'.join(codes)}{extension}"
+            with open(full.lower(), "rb") as fh:
+                return BytesIO(fh.read())
+        except:
+            print('failed to find', emoji, full)
+
+
+    def get_discord_emoji(self, id: int, /) -> Optional[BytesIO]:
+        raise NotImplementedError
+
 
 if __name__ == '__main__':
-    # Example usage:
     result = []
 
-    emoji_analyzer = EmojiAnalyzer('/System/Library/Fonts/seguiemj.ttf')
-    # emoji_analyzer = EmojiAnalyzer('/System/Library/Fonts/Apple Color Emoji.ttc')
+    # emoji_analyzer = EmojiAnalyzer('/Users/camerongarvey/Downloads/NotoColorEmoji.ttf', source=FileBaseSource)
+    emoji_analyzer = EmojiAnalyzer('/System/Library/Fonts/Apple Color Emoji.ttc', source=FileBaseSource)
 
+    red, green, blue = emoji_analyzer.get_average_color_text('◼️', show=True)
     for e in emoji.EMOJI_DATA:
-        red, green, blue, al = emoji_analyzer.get_average_color(e, background=(0, 0, 0, 255))
-        if red + green + blue > 100:
-            result.append([red, green, blue, e])
+        red, green, blue, al = emoji_analyzer.get_average_color_text(e, show=False, background=(255, 255, 255, 255))
+        if red == 255 and green == 255 and blue == 255:
+            print('shitty')
+            continue
+
+        result.append([red, green, blue, e])
 
     with open('emojis.json', 'w', encoding='utf-8') as f:
-        json.dump(result, f, ensure_ascii=True, indent=4)
+        json.dump(result, f, indent=4)
+
+    print("DONE!")
 
