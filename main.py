8938702ed763738ccf3b6e2f690ac0634594a4d5
CamGarvey <41130011+CamGarvey@users.noreply.github.com>
2024-03-03 10:05:42 +1100

    chore: push to repo

--- main.py
+++ main.py
@@ -8,34 +8,6 @@ from src.helpers.math import map_number
 from src.sources.text_base_source import TextBaseSource
 
 
-
-def get_average_color_from_domiant_colors(dominant_colors):
-    # Initialize weighted sums for each channel
-    weighted_red, weighted_green, weighted_blue = 0, 0, 0
-
-    # Define weights for each color (you can adjust these as needed)
-    weights = [1.0, 0.9, 0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2, 0.1]  # Weights for the first, second, and third colors
-
-    for i, (r, g, b) in enumerate(dominant_colors):
-        # Apply weights to the colors
-        weighted_red += r * weights[i]
-        weighted_green += g * weights[i]
-        weighted_blue += b * weights[i]
-
-    # Normalize the weighted sums
-    max_weight = sum(weights)
-    red = int(weighted_red / max_weight)
-    green = int(weighted_green / max_weight)
-    blue = int(weighted_blue / max_weight)
-
-    # Ensure the values are within the RGB range [0, 255]
-    red = max(0, min(255, red))
-    green = max(0, min(255, green))
-    blue = max(0, min(255, blue))
-
-    return red, green, blue
-
-
 def noto_path_builder(emoji: str):
     path = f"assets/128/"
     prefix = "emoji_u"
@@ -55,46 +27,100 @@ def noto_path_builder2(emoji: str):
     return f"assets/128/emoji_u{rgi(codepoints(emoji))}.png"
 
 
-def get_noto_ordering(group_filter=None):
+def get_google_emoji_ordering(exclude_groups=None, include_alternate=True):
     emoji_ordering = requests.get(
-        'https://cdn.jsdelivr.net/gh/googlefonts/emoji-metadata@main/emoji_15_0_ordering.json').json()
+        'https://cdn.jsdelivr.net/gh/googlefonts/emoji-metadata@main/emoji_15_1_ordering.json').json()
 
-    groups = emoji_ordering if not group_filter else filter(lambda x: x['group'] in group_filter, emoji_ordering)
+    groups = emoji_ordering if not exclude_groups else filter(lambda x: x['group'] not in exclude_groups,
+                                                              emoji_ordering)
     for group in groups:
         print(group['group'])
         for emoji in group['emoji']:
             unicode = render(emoji['base'])
             yield unicode
+            if include_alternate:
+                for alternate in emoji['alternates']:
+                    yield render(alternate)
 
 
+def get_apple_emoji_ordering(exclude_groups=None, include_alternate=True):
+    emoji_ordering = requests.get('https://cdn.jsdelivr.net/npm/emoji-datasource-apple@15.1.2/emoji.json').json()
 
-def get_dominant_colors_noto(background, show=False, group_filter=None):
-    result = []
-    font_size = 109
-    font_path = '/Users/camerongarvey/Documents/Repos/noto-emoji/fonts/NotoColorEmoji.ttf'
+    groups = []
+    uniquekeys = []
+    emoji_ordering = sorted(emoji_ordering, key=lambda x: x['category'])
+    for k, g in groupby(emoji_ordering, lambda x: x['category']):
+        groups.append(list(g))  # Store group iterator as a list
+        uniquekeys.append(k)
+    # groups = emoji_ordering if not exclude_groups else filter(lambda x: x not in exclude_groups, emoji_ordering)
+    for index, group in enumerate(groups):
+        print(uniquekeys[index])
+        for emoji in group:
+            unicode = render(emoji['google'])
+            yield unicode
+            if include_alternate:
+                for alternate in emoji['alternates']:
+                    yield render(alternate)
 
-    emoji_analyzer = EmojiAnalyzer(font_path, font_size, source=TextBaseSource(font_path, font_size=font_size, padding=10))
-    emoji_ordering = requests.get(
-        'https://cdn.jsdelivr.net/gh/googlefonts/emoji-metadata@main/emoji_15_0_ordering.json').json()
 
-    groups = group_filter if not group_filter else filter(lambda x: x['group'] in group_filter, emoji_ordering)
-    for group in groups:
-        print(group['group'])
-        for emoji in group['emoji']:
-            unicode = render(emoji['base'])
-            # red, green, blue, alpha = emoji_analyzer.get_average_color(unicode, background=background, show=show)
-            dominant_colors = emoji_analyzer.get_dominant_colors(unicode, background=background, show=show)
-            red, green, blue, alpha = get_average_color_from_domiant_colors(dominant_colors)
+def get_average_color_of_emojis_generator(get_ordering_func, emoji_analyzer: EmojiAnalyzer, exclude_groups=None,
+                                          background=None):
+    """
+    Uses google ordering so some emojis might be missing for apple
+    """
+    for unicode in get_ordering_func(exclude_groups):
+        try:
+            red, green, blue, alpha = emoji_analyzer.get_average_color(unicode, background=background)
+
+            if (red <= background[0] and green <= background[1] and blue <= background[2]) or alpha == 0:
+                print(f"{unicode} - Skipping; could not see it")
+                continue
 
             alpha = round(map_number(alpha, 0, 255, 0, 1), 2)
-            print(f"{unicode} - {' '.join(emoji['shortcodes'])} - [{red}, {green}, {blue} {alpha}]")
 
-            result.append({
+            print(unicode, [red, green, blue, alpha])
+            yield {
                 'emoji': unicode,
                 'rgb': [red, green, blue, alpha]
-            })
+            }
+        except Exception as e:
+            print(e)
+            print(f"{unicode} - Failed")
 
-    return result
+
+def get_average_color_of_emojis_generator2(emoji_analyzer: EmojiAnalyzer, background=None):
+    import emoji
+
+    for unicode in emoji.EMOJI_DATA:
+        try:
+            red, green, blue, alpha = emoji_analyzer.get_average_color(unicode, background=background)
+
+            if (red <= background[0] and green <= background[1] and blue <= background[2]) or alpha == 0:
+                print(f"{unicode} - Skipping; could not see it")
+                continue
+
+            alpha = round(map_number(alpha, 0, 255, 0, 1), 2)
+
+            print(unicode, [red, green, blue, alpha])
+            yield {
+                'emoji': unicode,
+                'rgb': [red, green, blue, alpha]
+            }
+        except Exception as e:
+            print(e)
+            print(f"{unicode} - Failed")
+
+
+def create_apple_emoji_analyzer(font_size):
+    font_path = '/System/Library/Fonts/Apple Color Emoji.ttc'
+    return EmojiAnalyzer(font_path, font_size, source=TextBaseSource(font_path, font_size=font_size))
+
+
+def create_noto_emoji_analyzer():
+    font_size = 109  # only font size
+    font_path = '/Users/camerongarvey/Documents/Repos/noto-emoji/fonts/NotoColorEmoji.ttf'
+    source = TextBaseSource(font_path, font_size=font_size, padding=20)
+    return EmojiAnalyzer(font_path, font_size, source=source)
 
 
 apple_font = [20, 26, 32, 40, 48, 52, 64, 96, 160]
@@ -107,7 +133,7 @@ def get_apple_from_font(background):
 
     emoji_analyzer = EmojiAnalyzer(font_path, font_size, source=TextBaseSource(font_path, font_size=font_size))
     emoji_ordering = requests.get(
-        'https://cdn.jsdelivr.net/gh/googlefonts/emoji-metadata@main/emoji_15_0_ordering.json').json()
+        'https://cdn.jsdelivr.net/gh/googlefonts/emoji-metadata@main/emoji_15_1_ordering.json').json()
 
     for group in emoji_ordering:
         print(group['group'])
@@ -137,16 +163,16 @@ def get_apple_from_font_2(background):
     font_path = '/System/Library/Fonts/Apple Color Emoji.ttc'
 
     emoji_analyzer = EmojiAnalyzer(font_path, font_size, source=TextBaseSource(font_path, font_size=font_size))
-    bad = 0
+    bad = []
     for unicode in emoji.EMOJI_DATA:
         try:
             red, green, blue, alpha = emoji_analyzer.get_average_color(unicode, background=background)
         except:
-            bad += 1
+            bad.append(unicode)
             continue
         if (red <= background[0] and green <= background[1] and blue <= background[2]) or alpha == 0:
             print('No emoji')
-            bad += 1
+            bad.append(unicode)
             continue
 
         alpha = round(map_number(alpha, 0, 255, 0, 1), 2)
@@ -162,21 +188,18 @@ def get_apple_from_font_2(background):
     return result
 
 
-def export_noto_emojis(font_size, output_size):
-    import emoji
-    font_path = '/Users/camerongarvey/Documents/Repos/noto-emoji/fonts/NotoColorEmoji.ttf'
-    emoji_analyzer = EmojiAnalyzer(font_path, font_size,
-                                   source=TextBaseSource(font_path, font_size=font_size, padding=10))
-    for unicode in get_noto_ordering():
+def export_noto_emojis(font_size: int, output_size: int):
+    emoji_analyzer = create_noto_emoji_analyzer()
+    for unicode in get_google_emoji_ordering():
         print('adding', unicode)
         try:
-            emoji_analyzer.download_png(unicode, (0, 0, 0, 0), f'emojis/noto/{output_size}/{unicode}.webp',
+            emoji_analyzer.download_png(unicode, (0, 0, 0, 0), f'emojis/noto/{output_size}/{unicode}.png',
                                         output_size=(output_size, output_size))
         except Exception as error:
             print('failed', error)
 
 
-def export_apple_emojis(font_size, output_size):
+def export_apple_emojis(font_size: int, output_size: int):
     import emoji
     font_path = '/System/Library/Fonts/Apple Color Emoji.ttc'
     emoji_analyzer = EmojiAnalyzer(font_path, font_size, source=TextBaseSource(font_path, font_size=font_size))
@@ -184,35 +207,34 @@ def export_apple_emojis(font_size, output_size):
     for unicode in emoji.EMOJI_DATA:
         print('adding', unicode)
         try:
-            emoji_analyzer.download_png(unicode, (0, 0, 0, 0), f'emojis/{output_size}/{unicode}.webp',
+            emoji_analyzer.download_png(unicode, (0, 0, 0, 0), f'emojis/apple/{output_size}/{unicode}.png',
                                         output_size=(output_size, output_size))
         except:
             print('failed')
 
 
-def find_font_sizes(emoji_analyzer, show=False):
+def find_font_sizes(font_path, show=False):
     sizes = []
     for i in range(1000):
         try:
-            emoji_analyzer.get_average_color('👨‍🏫', font_size=i, background=(0, 0, 0, 255), show=show)
+            source = TextBaseSource(font_path, font_size=i)
+            emoji_analyzer = EmojiAnalyzer(font_path, i, source=source)
+            emoji_analyzer.get_average_color('👨‍🏫', background=(0, 0, 0, 255), show=show)
             sizes.append(i)
         except Exception as e:
-            print(e)
             pass
     return sizes
 
 
 if __name__ == '__main__':
-    # export_apple_emojis(160, 128)
-    font_size = 109
-    output_size = 128
-    export_noto_emojis(font_size, font_size)
-    # result = get_noto_from_ttf((0, 0, 0, 0), show=True, group_filter=['Flags'])
-    # result = get_apple_from_font_2((0, 0, 0, 0))
-    #
-    # print(len(result))
-    #
-    # with open('emojis.json', "w", encoding="utf-8") as json_file:
-    #     json.dump(result, json_file, ensure_ascii=False, indent=4)
-    #
+    analyzer = create_apple_emoji_analyzer(160)
+    # analyzer.get_average_color('', (0,0,0,0), show=True)
+
+    results = list(get_average_color_of_emojis_generator(get_google_emoji_ordering, analyzer, background=(0, 0, 0, 0)))
+    with open('emojis.json', "w", encoding="utf-8") as json_file:
+        json.dump(results, json_file, ensure_ascii=False, indent=4)
+
     # print("DONE!")
+
+# These are the apple emojis that pillow doesn't like
+# {'google': 3827, 'appleEmoji': 4390}
